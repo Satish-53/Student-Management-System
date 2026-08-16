941d776e40b1cb94c44cf54a08c0d0670b92422d
Satish Yadav <satishyadav963172@gmail.com>
2026-08-16 17:52:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,33 @@ if (admissionForm) {
         alert("Admission submitted successfully!");
     });
 }
+// Student Login
+document.getElementById("studentLoginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const studentId = document.getElementById("student-id").value.trim();
+    const password = document.getElementById("student-password").value;
+
+    if (studentId === "student01" && password === "12345") {
+        alert("Student Login Successful!");
+        window.location.href = "student.html";
+    } else {
+        alert("Invalid Student ID or Password!");
+    }
+});
+
+
+// Admin Login
+document.getElementById("adminLoginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("adminUsername").value.trim();
+    const password = document.getElementById("adminPassword").value;
+
+    if (username === "admin" && password === "admin123") {
+        alert("Admin Login Successful!");
+        window.location.href = "admin.html";
+    } else {
+        alert("Invalid Admin Username or Password!");
+    }
+});
